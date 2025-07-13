9e2d0dcf80e8de7ff6140792c6885c8b7762b434
AbdulboisNigmatjonov17 <abdulboisnigmatjonov@gmail.com>
2025-07-13 19:39:48 +0500

    cart page UI

--- src/app/components/card/product-card/product-card.ts
+++ src/app/components/card/product-card/product-card.ts
@@ -1,17 +1,30 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { TruncatePipe } from '../../../pipes/truncate-pipe';
 import { Product } from '../../../models/product';
+import { CartService } from '../../../services/cart.service';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-product-card',
-  imports: [MatCardModule, MatGridListModule, TruncatePipe],
+  imports: [MatCardModule, TruncatePipe, MatSnackBarModule],
   templateUrl: './product-card.html',
   styleUrl: './product-card.css',
   standalone: true
 })
 export class ProductCard {
   @Input() item!: Product
+  private _snackBar = inject(MatSnackBar);
+  private cartService = inject(CartService)
 
+  addCart(product: Product): void {
+    const cartItem = { product, quantity: 1 };
+    this.cartService.addToCart(cartItem).subscribe({
+      next: () => this._snackBar.open('Product added to cart', '', {
+        duration: 2000,
+        horizontalPosition: 'right',
+        verticalPosition: 'top'
+      })
+    })
+  }
 }
